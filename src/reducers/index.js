33b6b3f99e8b6e45503fad25212d35915ecb1e0a
azizmashkour <azizmashkour@gmail.com>
2019-02-12 14:37:57 +0100

    feat: import new action creators, reducer and types

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import layoutReducer from './layout';
 import appInstanceReducer from './appInstance';
 import contextReducer from './context';
 import userReducer from './users';
+import simulationReducer from './simulation';
 
 export default combineReducers({
   layout: layoutReducer,
   appInstance: appInstanceReducer,
   context: contextReducer,
   user: userReducer,
+  simulation: simulationReducer,
 });
