67aaca42a984fef6801a112abff0d216262ae6dc
azizmashkour <azizmashkour@gmail.com>
2019-02-12 18:23:09 +0100

    feat: create semi line component

--- src/components/visualizer/cases/SemiLine.js
+++ src/components/visualizer/cases/SemiLine.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Circle, Layer, Line } from 'react-konva';
 
 const SemiLine = () => (
-  <div className="semiline-component">
-    This is our SemiLine component
-  </div>
+  <Layer>
+    <Line
+      points={[100, 300, 900, 150]}
+      stroke={5}
+      strokeWidth={10}
+    />
+    <Circle
+      x={100}
+      y={300}
+    />
+  </Layer>
 );
 
 export default SemiLine;
