5501b44fb9f09e20ff9c03d2f0b5cf338104e928
azizmashkour <azizmashkour@gmail.com>
2019-02-12 11:47:20 +0100

    feat: add use case buttons

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
 
 const Resume = ({
   t,
 }) => (
   <div className="resume-container">
-    <h1>{t('Lab Title')}</h1>
-    <p className="description-definition">
-      {t('Lab Explanation')}
-    </p>
+    <div className="description-content">
+      <h2>{t('Description')}</h2>
+      <p>
+        {t('Lab Explanation')}
+      </p>
+      <hr className="separator-line" />
+    </div>
+    <div className="use-cases">
+      <Button
+        outline
+        color="secondary"
+        className="use-case"
+      >
+        {t('Line')}
+      </Button>
+      <br />
+      <Button
+        outline
+        color="secondary"
+        className="use-case semi-line"
+      >
+        {t('Semi-line')}
+      </Button>
+      <br />
+      <Button
+        outline
+        color="secondary"
+        className="use-case"
+      >
+        {t('Segment')}
+      </Button>
+    </div>
   </div>
 );
 
