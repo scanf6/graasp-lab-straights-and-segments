58eb06a8bdab0012547237be0e17f3a34896fdb9
azizmashkour <azizmashkour@gmail.com>
2019-02-13 13:12:03 +0100

    feat: draw each use case lines and add texts

--- src/components/visualizer/cases/Liner.js
+++ src/components/visualizer/cases/Liner.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { Line } from 'react-konva';
+import PropTypes from 'prop-types';
+import { Line, Layer, Stage } from 'react-konva';
+import {
+  stroke,
+  lineStrokeWidth,
+} from '../../../config/properties';
 
-const Liner = () => (
-  <Line
-    points={[100, 500, 700, 50]}
-    stroke={5}
-    strokeWidth={10}
-  />
+const Liner = ({ scale, renderVerticalGrid, renderHorizontalGrid }) => (
+  <Stage
+    width={window.innerWidth}
+    height={window.innerHeight}
+    scaleX={scale}
+    scaleY={scale}
+  >
+    <Layer>
+      {renderHorizontalGrid}
+      {renderVerticalGrid}
+      <Line
+        points={[100, 400, 700, 50]}
+        stroke={stroke}
+        strokeWidth={lineStrokeWidth}
+      />
+    </Layer>
+  </Stage>
 );
+
+Liner.propTypes = {
+  scale: PropTypes.number.isRequired,
+  renderVerticalGrid: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  renderHorizontalGrid: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
 
 export default Liner;
--- src/components/visualizer/cases/SemiLine.js
+++ src/components/visualizer/cases/SemiLine.js
@@ -1,18 +1,93 @@
 import React from 'react';
-import { Circle, Layer, Line } from 'react-konva';
+import PropTypes from 'prop-types';
+import {
+  Circle,
+  Layer,
+  Line,
+  Text,
+  Stage,
+} from 'react-konva';
+import {
+  fill,
+  stroke,
+  fontSize,
+  circleRadius,
+  circleShadowBlur,
+  circleStrokeWidth,
+  lineStrokeWidth,
+} from '../../../config/properties';
+import {
+  CIRCLE_COMMON_X_0,
+  CIRCLE_COMMON_X_1,
+  CIRCLE_COMMON_Y_0,
+  CIRCLE_COMMON_Y_1,
+  CIRCLE_TEXT_X_0,
+  CIRCLE_TEXT_X_1,
+  CIRCLE_TEXT_Y_0,
+  CIRCLE_TEXT_Y_1,
+  COMMON_LINE_POINTS,
+} from '../../../config/coordinates';
 
-const SemiLine = () => (
-  <Layer>
-    <Line
-      points={[100, 300, 900, 150]}
-      stroke={5}
-      strokeWidth={10}
-    />
-    <Circle
-      x={100}
-      y={300}
-    />
-  </Layer>
+const SemiLine = ({ scale, renderVerticalGrid, renderHorizontalGrid }) => (
+  <Stage
+    width={window.innerWidth}
+    height={window.innerHeight}
+    scaleX={scale}
+    scaleY={scale}
+  >
+    <Layer>
+      {renderHorizontalGrid}
+      {renderVerticalGrid}
+      <Line
+        points={COMMON_LINE_POINTS}
+        stroke={stroke}
+        strokeWidth={lineStrokeWidth}
+      />
+      <Line
+        points={[600, 200, 800, 120]}
+        stroke={stroke}
+        strokeWidth={lineStrokeWidth}
+      />
+      <Circle
+        x={CIRCLE_COMMON_X_0}
+        y={CIRCLE_COMMON_Y_0}
+        stroke={stroke}
+        fill={fill}
+        radius={circleRadius}
+        strokeWidth={circleStrokeWidth}
+        shadowBlur={circleShadowBlur}
+      />
+      <Circle
+        x={CIRCLE_COMMON_X_1}
+        y={CIRCLE_COMMON_Y_1}
+        stroke={stroke}
+        fill={fill}
+        radius={circleRadius}
+        strokeWidth={circleStrokeWidth}
+        shadowBlur={circleShadowBlur}
+      />
+      <Text
+        x={CIRCLE_TEXT_X_0}
+        y={CIRCLE_TEXT_Y_0}
+        text="A"
+        fontSize={fontSize}
+        fill={fill}
+      />
+      <Text
+        x={CIRCLE_TEXT_X_1}
+        y={CIRCLE_TEXT_Y_1}
+        text="B"
+        fontSize={fontSize}
+        fill={fill}
+      />
+    </Layer>
+  </Stage>
 );
+
+SemiLine.propTypes = {
+  scale: PropTypes.number.isRequired,
+  renderVerticalGrid: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  renderHorizontalGrid: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
 
 export default SemiLine;
