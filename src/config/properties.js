b20436bd9d87949e8b8b53bc60ec8f8c9d6e6b65
azizmashkour <azizmashkour@gmail.com>
2019-02-13 12:18:55 +0100

    feat: add coordinates and properties config files

---
+++ src/config/properties.js
@@ -0,0 +1,7 @@
+export const fill = '#000000';
+export const stroke = '#000000';
+export const fontSize = 40;
+export const circleRadius = 5;
+export const circleShadowBlur = 5;
+export const circleStrokeWidth = 15;
+export const lineStrokeWidth = 10;
