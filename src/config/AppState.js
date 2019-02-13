5d1c9466ac0a50b4235d9dd865cb55a39c6ab257
azizmashkour <azizmashkour@gmail.com>
2019-02-13 17:08:01 +0100

    feat: add missing state params

--- src/config/AppState.js
+++ src/config/AppState.js
@@ -2,6 +2,9 @@ export const AppState = {
   obserViewActive: true,
   showTitle: true,
   openModal: false,
+  isMouseInside: false,
+  lineCoordinates: [100, 400, 600, 200],
+  circleCoordinates: [600, 200],
 };
 
 export default AppState;
