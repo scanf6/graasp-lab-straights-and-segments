7e68a97a0f6daa3c2b6b870d68a2b9649ae0d9bf
azizmashkour <azizmashkour@gmail.com>
2019-02-15 15:56:42 +0100

    feat: add missing segment initial shape state

--- src/config/AppState.js
+++ src/config/AppState.js
@@ -7,6 +7,7 @@ export const AppState = {
   circleCoordinates: [600, 200],
 
   shapes: [], // list of dimensions to be rendered as shapes
+  segmentShapes: [], // list of dimensions to be rendered as shapes for the segment
   isDrawing: false, // in the process of drawing a shape
   isDrawingMode: false, // allow shapes to be drawn
 };
